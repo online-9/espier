4dffc812ee112cba4d077134060959eae6004a45
Parul Sethi <parul1sethi@gmail.com>
2016-07-08 21:26:31 +0530

    scrapes for multiple queries

--- scrape_images.py
+++ scrape_images.py
@@ -1,100 +1,127 @@
-import time
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
+import time
 import base64
 import re
 import glob
 import urllib
 import os
 
-QUERY = raw_input('Query string: ')
-DOWNLOAD_DIR = os.path.dirname(__file__)
+class ScrapeImages(object):
+	''' Scrapes Google image search results for multiple queries
+	 ''' 
 
-target_url = "https://www.google.com/search?as_st=y&tbm=isch&hl=en&as_q=%s&as_epq=&as_oq=&as_eq=&cr=&as_sitesearch=&safe=images&tbs=isz:m" % QUERY
+	def __init__(self):
+		self.DOWNLOAD_DIR = os.path.dirname(__file__)
+		self.image_xpath = "//img[@class='rg_i']"
+		self.driver = webdriver.Firefox()
 
-image_xpath = "//img[@class='rg_i']"
+	def target_url(self,query):
+		return "https://www.google.com/search?as_st=y&tbm=isch&hl=en&as_q=%s&as_epq=&as_oq=&as_eq=&cr=&as_sitesearch=&safe=images&tbs=isz:m" % query
 
-driver = webdriver.Firefox()
-driver.get(target_url)
+	def scrape(self,query):
+		# Scrape all results returned from current page
+		self.driver.get(self.target_url(query))
+		self.scroll_down
+		prev_file_no = 0
+		imgs = self.driver.find_elements_by_xpath(self.image_xpath)
+		# the first image_xpath returns only 100 images,so repeat find_elements_by_xpath again and again
+		while prev_file_no < 1000 and len(imgs) > prev_file_no:
+			self.scroll_down
+			imgs = self.driver.find_elements_by_xpath(self.image_xpath)
+			print('new loop. found %i images, prev_file_no was %i' % (len(imgs), prev_file_no))
+			for file_no, img_el in enumerate(imgs[prev_file_no:]):
+				self.save_img(img_el,file_no+prev_file_no,foldername=query)
+			prev_file_no = len(imgs)
 
-# modified from http://sqa.stackexchange.com/questions/3499/how-to-scroll-to-bottom-of-page-in-selenium-ide
-def scroll_down():
-	# define initial page height for 'while' loop
-	last_height = driver.execute_script("return document.body.scrollHeight")
-	while True:
-		driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-		time.sleep(8)
+	def create_folder(self,foldername):
+		# Create folders for corresponding queries
+		if not os.path.exists(foldername):
+			os.makedirs(os.path.join(self.DOWNLOAD_DIR,foldername))
+
+	def read_queries(self,filename):
+		# Retrieve image queries from query file
+		with open(filename,'r') as q:
+			query_list = [line.strip().split() for line in q.readlines()]
+		return query_list
+
+	def run(self,filename):
+		# Scrapes over all queries in the loop 
+		query_list = self.read_queries(filename)
+		for query in query_list:
+			print(query)
+			# images corresponding to every query will be stored in a folder with the same name
+			self.create_folder(foldername=query[0])
+			self.scrape(query[0])
+
+	# modified from http://sqa.stackexchange.com/questions/3499/how-to-scroll-to-bottom-of-page-in-selenium-ide
+	@property
+	def scroll_down(self):
+		# define initial page height for 'while' loop
+		last_height = self.driver.execute_script("return document.body.scrollHeight")
+		while True:
+			self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+			time.sleep(8)
+			try:
+				self.driver.find_element_by_id('smb').click()
+			except:
+				print('no See More button found...')
+			new_height = self.driver.execute_script("return document.body.scrollHeight")
+
+			if new_height == last_height:
+				break
+			else:
+				last_height = new_height
+
+	def hover(self,el):
+		# Hovers to input element
+		ActionChains(self.driver).move_to_element(el).perform()
+
+	def right_click_save_as(self,el):
+		# ActionChains queues up these tasks to perform right click
+		ActionChains(self.driver).move_to_element(el).context_click(el).send_keys('V').perform()
+
+	def save_img_src(self,el,file_no,foldername,sleep_time=0.25):
+		# Retrieve from source url
+		base = el.get_attribute('src')
+		# just guess jpeg,no file ext in url
+		file_name = foldername+"/"+str(file_no)+".jpeg"
 		try:
-			driver.find_element_by_id('smb').click()
-		except:
-			print 'no See More button found...'
+			urllib.urlretrieve(base, file_name)
+			print('wrote ',file_name)
+		except IOError as e:
+			print('save from image source error',e)
+		time.sleep(sleep_time)
 
-		new_height = driver.execute_script("return document.body.scrollHeight")
+	def save_img(self,el,file_no,foldername,sleep_time=0.25):
+		# Hovers to current image and save it
+		self.hover(el)
+		time.sleep(0.25)
 
-		if new_height == last_height:
-			break
-		else:
-			last_height = new_height
+		base = el.get_attribute('src')
+		if not base:
+			print('no img', file_no)
+			return
 
+		base_clean = base[base.find(','):]
+		try:
+			base_filetype = re.findall(r'image/(.*);', base)[0]
+		except IndexError:
+			print('no image extension,try to save from src', file_no)
+			save_img_src(el,file_no,foldername)
+			return
 
-def hover(el):
-	ActionChains(driver).move_to_element(el).perform()
+		file_name = foldername+"/"+str(file_no)+"."+ base_filetype
+		with open(os.path.join(self.DOWNLOAD_DIR,file_name), 'w') as f:
+			f.write(base64.decodestring(base_clean))
 
-def right_click_save_as(el):
-	ActionChains(driver).move_to_element(el) \
-	.context_click(el) \
-	.send_keys('V') \
-	.perform()
-
-def save_img_src(el, file_no, sleep_time=0.25):
-	base = el.get_attribute('src')
-	# just guess jpeg, probably no file ext in url...
-	file_name_full = 'panda'+"/"+str(file_no)+".jpeg"
-	try:
-		urllib.urlretrieve(base, file_name_full)
-		print 'wrote from url %s' % file_name_full
-	except IOError as e:
-		print 'Bad URL?', e
-
-	time.sleep(sleep_time)
-
-
-def dl_base64_img(el, file_no, sleep_time=0.20):
-	hover(el)
-	time.sleep(0.20)
-
-	base = el.get_attribute('src')
-	if not base:
-		print 'no img', file_no
-		return
-
-	base_clean = base[base.find(','):]
-	try:
-		base_filetype = re.findall(r'image/(.*);', base)[0]
-	except IndexError:
-		print 'no img filetype... trying to save src', file_no
-		save_img_src(el, file_no)
-		return
-
-	file_name_full = 'panda'+"/"+str(file_no)+"."+ base_filetype
-	with open(os.path.join(DOWNLOAD_DIR,file_name_full), 'w') as f:
-		f.write(base64.decodestring(base_clean))
-
-	print 'wrote %s' % file_name_full
-	time.sleep(sleep_time)
+		print('wrote ',file_name)
+		time.sleep(sleep_time)
 
 if __name__ == "__main__":
-	scroll_down()
 
-	prev_file_no = 0
-	imgs = driver.find_elements_by_xpath(image_xpath)
-	# the first image_xpath returns only 100 images,so repeat find_elements_by_xpath again and again
-	while prev_file_no < 1000 and len(imgs) > prev_file_no:
-		scroll_down()
-		imgs = driver.find_elements_by_xpath(image_xpath)
-		print 'new loop. found %i images, prev_file_no was %i' % (len(imgs), prev_file_no)
-		for file_no, img_el in enumerate(imgs[prev_file_no:]):
-			dl_base64_img(img_el, file_no+prev_file_no)
-		prev_file_no = len(imgs)
+	# create a queries.txt 
+	obj = ScrapeImages()
+	obj.run('queries.txt')
